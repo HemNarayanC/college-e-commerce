f618eef084c67658ffb20ceb5f962c91a4eb9ae8
Hem Narayan Chaudhary <hemnarayan.chaudhary.insight@gmail.com>
2025-05-13 12:16:51 +0545

    added phone

--- Frontend/src/components/Header/TopHeader.jsx
+++ Frontend/src/components/Header/TopHeader.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
+import React from "react";
+import { FaPhone } from "react-icons/fa";
 
 const TopHeader = () => {
   return (
     <div className="container mx-auto px-4 py-2 flex justify-between items-center">
-        <div className="flex items-center space-x-2 text-xs text-gray-600">
-            <span className="flex gap-0.5 items-center justify-center">
-                Top Header
-            </span>
-        </div>
+      <div className="flex -center space-x-2 text-xs text-gray-600">
+        <span className="flex gap-0.5 items-center justify-center">
+          <FaPhone />
+          <p> +1 (800) 123-4567</p>
+        </span>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default TopHeader
+export default TopHeader;
