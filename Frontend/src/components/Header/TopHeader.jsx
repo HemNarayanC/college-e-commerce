b65cd37e73f2f8ca4ca7d34e9f81867f37e713e6
Hem Narayan Chaudhary <hemnarayan.chaudhary.insight@gmail.com>
2025-05-13 12:03:17 +0545

    top component of header

---
+++ Frontend/src/components/Header/TopHeader.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const TopHeader = () => {
+  return (
+    <div className="container mx-auto px-4 py-2 flex justify-between items-center">
+        <div className="flex items-center space-x-2 text-xs text-gray-600">
+            <span className="flex gap-0.5 items-center justify-center">
+                Top Header
+            </span>
+        </div>
+    </div>
+  )
+}
+
+export default TopHeader
