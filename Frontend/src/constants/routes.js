6232fc78c2f11b3eabc3b7c9ae3d768e5dd3a96b
Hem Narayan Chaudhary <hemnarayan.chaudhary.insight@gmail.com>
2025-05-19 12:10:29 +0545

    defined routes constants for navigation

---
+++ Frontend/src/constants/routes.js
@@ -0,0 +1,12 @@
+const HOME_ROUTE = "/"
+const ABOUT_ROUTE = "/about"
+const CONTACT_ROUTE = "/contact"
+const STORE_ROUTE = "/store-manager"
+const VENDOR_REGISTER_ROUTE = "/vendor-register"
+const VENDOR_MEMBERSHIP_ROUTE = "/vendor-membership"
+const SHOP_ROUTE = "/shop"
+const LOGIN_ROUTE = "/auth/login"
+const REGISTER_ROUTE = "/auth/register"
+
+
+export {HOME_ROUTE, ABOUT_ROUTE, CONTACT_ROUTE, VENDOR_MEMBERSHIP_ROUTE, VENDOR_REGISTER_ROUTE, STORE_ROUTE, SHOP_ROUTE, LOGIN_ROUTE, REGISTER_ROUTE}
