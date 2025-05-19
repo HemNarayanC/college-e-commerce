78a5323ee4ecae1b158277780b49fa685828bc49
Hem Narayan Chaudhary <hemnarayan.chaudhary.insight@gmail.com>
2025-05-19 12:20:21 +0545

    routes and labels

---
+++ Frontend/src/constants/navMenu.js
@@ -0,0 +1,34 @@
+import { ABOUT_ROUTE, CONTACT_ROUTE, HOME_ROUTE, VENDOR_MEMBERSHIP_ROUTE,VENDOR_REGISTER_ROUTE,SHOP_ROUTE, STORE_ROUTE } from "./routes";
+
+const navMenu = [
+    {
+      route: HOME_ROUTE,
+      label: "Home"
+    },
+    {
+        route: SHOP_ROUTE,
+        label: "Shop"
+    },
+    {
+        route: STORE_ROUTE,
+      label: "Store Manager"
+    },
+    {
+        route: VENDOR_REGISTER_ROUTE,
+        label: "Vendor Registration"
+    },
+    {
+        route: VENDOR_MEMBERSHIP_ROUTE,
+        label: "Vendor MemberShip"
+    },
+    {
+      route: ABOUT_ROUTE,
+      label: "About Us"
+    },
+    {
+      route: CONTACT_ROUTE,
+      label: "Contact"
+    },
+  ]
+
+  export default navMenu;
