4d6e66f3a39c0067a3a795db0dcab3c3cb2a05f8
Hem Narayan Chaudhary <hemnarayan.chaudhary.insight@gmail.com>
2025-05-19 12:32:02 +0545

    user profile and dropdown

--- Frontend/src/components/Header/AuthMenu.jsx
+++ Frontend/src/components/Header/AuthMenu.jsx
@@ -1,11 +1,45 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import { FaUserCircle } from "react-icons/fa";
 
 const AuthMenu = () => {
   return (
     <div>
-        
+      <div className="relative group">
+        <button className="flex flex-col items-center cursor-pointer">
+          <FaUserCircle />
+          <span className="text-xs mt-1">Account</span>
+        </button>
+        <div className="absolute right-0 top-full w-48 bg-white shadow-lg rounded-md invisible opacity-0 group-hover:visible group-hover:opacity-100 transition-opacity duration-200 z-10">
+          <div className="py-1">
+            <Link to="#" className="block px-4 py-2 text-sm hover:bg-gray-100">
+              My Profile
+            </Link>
+            <Link
+              to={"#"}
+              className="block px-4 py-2 text-sm hover:bg-gray-100"
+            >
+              Carts
+            </Link>
+            <Link to="#" className="block px-4 py-2 text-sm hover:bg-gray-100">
+              Wishlist
+            </Link>
+            <Link to="#" className="block px-4 py-2 text-sm hover:bg-gray-100">
+              Settings
+            </Link>
+            <hr className="my-1" />
+            <Link
+              to="#"
+              className="block px-4 py-2 text-sm hover:bg-gray-100"
+              onClick=""
+            >
+              Logout
+            </Link>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default AuthMenu
+export default AuthMenu;
