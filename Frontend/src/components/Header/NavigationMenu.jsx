a31c5282cc298dc19b503f6acc988b7fa74195b4
Hem Narayan Chaudhary <hemnarayan.chaudhary.insight@gmail.com>
2025-05-25 12:35:00 +0545

    Add logic to update slider position based on active route

--- Frontend/src/components/Header/NavigationMenu.jsx
+++ Frontend/src/components/Header/NavigationMenu.jsx
@@ -1,30 +1,52 @@
+import { useEffect, useRef, useState } from "react";
+import { NavLink, useLocation } from "react-router-dom";
 import navMenu from "../../constants/navMenu";
-import { NavLink } from "react-router-dom";
 
 const NavigationMenu = () => {
+  const location = useLocation();
+  const containerRef = useRef(null);
+  const [sliderStyle, setSliderStyle] = useState({});
+  const linkRefs = useRef([]);
+
+  useEffect(() => {
+    const index = navMenu.findIndex((menu) => menu.route === location.pathname);
+    const link = linkRefs.current[index];
+
+    if (link) {
+      const { offsetLeft, offsetWidth } = link;
+      const extraPadding = 20;
+      setSliderStyle({
+        left: `${offsetLeft - extraPadding / 3}px`,
+        width: `${offsetWidth + extraPadding}px`,
+      });
+    }
+  }, [location.pathname]);
+
   return (
-    <div className="w-full flex justify-center h-12 items-center bg-[#6D9886]">
+    <div className="w-full flex justify-center h-12 items-center relative z-50">
       <header>
-        {/* Navigation */}
-        <nav className="text-[#FBF9F7]">
-          <div className="flex justify-around gap-8">
-            {navMenu.map((menu) => {
-              return (
-                <NavLink
-                  className={({ isActive }) =>
-                    `flex text-[15px] ${
-                      isActive
-                        ? "text-[#E1C699]" // Soft gold for active
-                        : "hover:text-[#FBF9F7]" // Creamy white on hover
-                    }`
-                  }
-                  key={menu.route}
-                  to={menu.route}
-                >
-                  {menu.label}
-                </NavLink>
-              );
-            })}
+        <nav className="text-[#1e714f] relative">
+          <div
+            ref={containerRef}
+            className="relative flex justify-around gap-8"
+          >
+            {/* Glassmorphism slider */}
+            <div
+              className="absolute top-0 bottom-0 rounded-md transition-all duration-300 backdrop-blur-sm bg-[#6D9886]/10 shadow-inner z-0"
+              style={sliderStyle}
+            />
+
+            {/* Navigation Links */}
+            {navMenu.map((menu, i) => (
+              <NavLink
+                ref={(el) => (linkRefs.current[i] = el)}
+                key={menu.route}
+                to={menu.route}
+                className="relative z-10 px-2 py-1 text-[16px]"
+              >
+                {menu.label}
+              </NavLink>
+            ))}
           </div>
         </nav>
       </header>
