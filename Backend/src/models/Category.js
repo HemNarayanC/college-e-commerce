531633e5d24d7ed390358726ef503a4499f8d618
Hem Narayan Chaudhary <hemnarayan.chaudhary.insight@gmail.com>
2025-06-18 15:58:59 +0545

    category schema

---
+++ Backend/src/models/Category.js
@@ -0,0 +1,7 @@
+import mongoose, { mongo } from "mongoose";
+
+const CategorySchema = new mongoose.Schema({
+    name: { type: String, required: true, unique: true },
+})
+
+export default mongoose.model("Category", CategorySchema);
