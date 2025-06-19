fbb06dd0d6315051edfdc6a57a4d5546b87f722d
Hem Narayan Chaudhary <hemnarayan.chaudhary.insight@gmail.com>
2025-06-19 14:16:09 +0545

    product add controller

---
+++ Backend/src/controllers/productController.js
@@ -0,0 +1,22 @@
+// controllers/productController.js
+import productService from "../services/productService.js";
+
+// Add a new product
+const addProduct = async (req, res) => {
+  try {
+    const vendorId = req.user.vendorId; // comes from token
+    const productData = req.body;
+    // console.log("Adding product for vendor:", vendorId, "with data:", productData);
+
+    const savedProduct = await productService.addProduct(vendorId, productData);
+
+    res.status(201).json({
+      message: "Product added successfully",
+      product: savedProduct,
+    });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+export { addProduct };
