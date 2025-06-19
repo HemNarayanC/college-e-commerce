fa877601dfe5d3650ea981382b684a6d1440a604
Hem Narayan Chaudhary <hemnarayan.chaudhary.insight@gmail.com>
2025-06-19 14:23:15 +0545

    controller for approving pending vendor

---
+++ Backend/src/controllers/adminVendorController.js
@@ -0,0 +1,35 @@
+import User from "../models/User.js";
+import Vendor from "../models/Vendor.js";
+
+const approveVendor = async (req, res) => {
+  try {
+    const vendorId = req.params.vendorId;
+    const { status } = req.body; // get status from request body
+
+    if (!status) {
+      return res.status(400).json({ message: "Status is required." });
+    }
+
+    const vendor = await Vendor.findById(vendorId);
+    if (!vendor) return res.status(404).json({ message: "Vendor not found." });
+
+    // Update vendor status
+    vendor.status = status;
+    await vendor.save();
+
+    // If status is approved, promote user role
+    if (status === "approved") {
+      const user = await User.findById(vendor.userId);
+      if (user && user.role === "customer") {
+        user.role = "vendor";
+        await user.save();
+      }
+    }
+
+    res.status(200).json({ message: `Vendor status updated to ${status}.` });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export { approveVendor };
