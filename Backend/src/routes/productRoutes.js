0283a76d8eb93718a3bf42225031529de27fa3f3
Hem Narayan Chaudhary <hemnarayan.chaudhary.insight@gmail.com>
2025-06-19 14:14:25 +0545

    product search route

--- Backend/src/routes/productRoutes.js
+++ Backend/src/routes/productRoutes.js
@@ -1,11 +1,17 @@
-
 import express from "express";
 import { addCategory } from "../controllers/categroyController.js";
 import authenticateVendor from "../middlewares/vendorMiddleware.js";
-import { addProduct, getProductById, listProductsHandler, toggleProductStatus } from "../controllers/productController.js";
+import {
+  addProduct,
+  getProductById,
+  listProductsHandler,
+  searchProductsController,
+  toggleProductStatus,
+} from "../controllers/productController.js";
 
 const router = express.Router();
 
+router.get("/search", searchProductsController);
 router.post("/addCategory", authenticateVendor, addCategory);
 router.get("/", listProductsHandler);
 router.get("/:id", getProductById);
