649904f6085dce01c43c410e365c22eb454e5e52
Hem Narayan Chaudhary <hemnarayan.chaudhary.insight@gmail.com>
2025-06-21 19:40:41 +0545

    order routes

---
+++ Backend/src/routes/orderRoutes.js
@@ -0,0 +1,12 @@
+import express from "express";
+import auth from "../middlewares/authMiddleware.js";
+import authenticateVendor from "../middlewares/vendorMiddleware.js";
+import { createOrder, getVendorOrders, updateOrderItemStatus } from "../controllers/orderController.js";
+
+const router = express.Router();
+
+router.post("/place-order", auth, createOrder);
+router.get("/vendor-orders-id", authenticateVendor, getVendorOrders);
+router.patch("/vendor/item-status", authenticateVendor, updateOrderItemStatus);
+
+export default router;
