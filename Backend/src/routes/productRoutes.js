51acb23f3a133a01329ead2154555ff9384b3bcd
Hem Narayan Chaudhary <hemnarayan.chaudhary.insight@gmail.com>
2025-06-21 07:17:46 +0545

    Add product image upload middleware

--- Backend/src/routes/productRoutes.js
+++ Backend/src/routes/productRoutes.js
@@ -3,19 +3,31 @@ import { addCategory } from "../controllers/categroyController.js";
 import authenticateVendor from "../middlewares/vendorMiddleware.js";
 import {
   addProduct,
+  deleteProduct,
   getProductById,
   listProductsHandler,
+  listVendorProducts,
   searchProductsController,
   toggleProductStatus,
+  updateProduct,
 } from "../controllers/productController.js";
+import { uploadProductImage } from "../services/cloudinaryConfig.js";
 
 const router = express.Router();
 
 router.get("/search", searchProductsController);
 router.post("/addCategory", authenticateVendor, addCategory);
 router.get("/", listProductsHandler);
+router.get("/vendor", authenticateVendor, listVendorProducts);
 router.get("/:id", getProductById);
-router.post("/addProduct", authenticateVendor, addProduct);
+router.post(
+  "/addProduct",
+  uploadProductImage.array("images", 5),
+  authenticateVendor,
+  addProduct
+);
 router.patch("/:id/status", authenticateVendor, toggleProductStatus);
+router.put("/:id", authenticateVendor, updateProduct);
+router.delete("/:id", authenticateVendor, deleteProduct);
 
 export default router;
