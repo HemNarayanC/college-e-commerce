d7cf7ca1d9b937602b3f7ba33035034a41094462
Hem Narayan Chaudhary <hemnarayan.chaudhary.insight@gmail.com>
2025-06-23 18:33:06 +0545

    notification model

---
+++ Backend/src/models/Notification.js
@@ -0,0 +1,23 @@
+import mongoose, { Types } from "mongoose";
+
+const NotificationSchema = new mongoose.Schema({
+  userId: { type: Types.ObjectId, ref: "User" }, // For customers
+  vendorId: { type: Types.ObjectId, ref: "Vendor" }, // For vendors
+  admin: { type: Boolean, default: false }, // For admin users
+
+  title: { type: String, required: true },
+  message: { type: String, required: true },
+  link: { type: String }, // Optional: to order, product, ticket, etc.
+
+  isRead: { type: Boolean, default: false },
+
+  type: {
+    type: String,
+    enum: ["order_placed", "order_delivered", "review", "ticket", "general"],
+    default: "general",
+  },
+
+  createdAt: { type: Date, default: Date.now },
+});
+
+export default mongoose.model("Notification", NotificationSchema);
