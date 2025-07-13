76d84a720429998b3088278cc32ec20e54bb7ce0
Hem Narayan Chaudhary <hemnarayan.chaudhary.insight@gmail.com>
2025-07-13 06:26:35 +0545

    add vendor dashboard api fetching

---
+++ Frontend/src/api/vendorDashboardApi.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+import { config } from "../config/config";
+
+const getVendorDashboardData = async (token) => {
+  const response = await axios.get(
+    `${config.baseApiUrl}/vendor/dashboard`,
+    {
+      headers: {
+        Authorization: token,
+      },
+      withCredentials: true,
+    }
+  );
+  console.log("Response for Vendor Dashboard", response)
+  return response.data;
+};
+
+export {getVendorDashboardData}
