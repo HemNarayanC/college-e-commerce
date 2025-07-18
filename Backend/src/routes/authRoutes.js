56421799e0bff71d9464ba3d4baf344f490647c8
Hem Narayan Chaudhary <hemnarayan.chaudhary.insight@gmail.com>
2025-07-18 22:42:03 +0545

    add auth routes with logout and flag review

--- Backend/src/routes/authRoutes.js
+++ Backend/src/routes/authRoutes.js
@@ -1,15 +1,18 @@
 import express from 'express';
-import { login, register } from '../controllers/authController.js';
+import { login, logout, register } from '../controllers/authController.js';
 import authenticate from '../middlewares/authMiddleware.js';
 import { registerVendor } from '../controllers/vendorController.js';
 // import { multer, storage } from '../middlewares/multerMiddleware.js';
 import { uploadUserProfile } from '../services/cloudinaryConfig.js';
+import { flagReview } from '../controllers/reviewController.js';
+import authenticateUserOrVendor from '../middlewares/sharedFlagMiddleware.js';
 
 const router = express.Router();
 // const upload = multer({storage})
 
 router.post('/register', uploadUserProfile.single('profileImage'), register);
 router.post('/login', login);
+router.post('/logout', authenticate, logout);
 router.get('/profile', authenticate, (req, res) => {
   res.json({
     message: 'User profile',
@@ -17,5 +20,6 @@ router.get('/profile', authenticate, (req, res) => {
   });
 });
 router.post("/register", authenticate, registerVendor);
+router.post("/:reviewId/flag", authenticateUserOrVendor, flagReview);
 
 export default router;
