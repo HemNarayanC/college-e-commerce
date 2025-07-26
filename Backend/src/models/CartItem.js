82a569992427ee3f5b9b1f29a12fb06ded2147fe
Hem Narayan Chaudhary <hemnarayan.chaudhary.insight@gmail.com>
2025-07-26 10:48:25 +0545

    cart item schema

--- Backend/src/models/CartItem.js
+++ Backend/src/models/CartItem.js
@@ -1,9 +1,10 @@
 import mongoose from "mongoose";
 
 const CartItemSchema = new mongoose.Schema({
-  userId: { type: Types.ObjectId, ref: "User", required: true },
-  productId: { type: Types.ObjectId, ref: "Product", required: true },
-  quantity: { type: Number, required: true, min: 1 }
+  userId:    { type: mongoose.Types.ObjectId, ref: "User",   required: true },
+  productId: { type: mongoose.Types.ObjectId, ref: "Product",required: true },
+  variantId: { type: mongoose.Types.ObjectId, default: null },
+  quantity:  { type: Number, default: 1, min: 1 }
 });
 
 export default mongoose.model("CartItem", CartItemSchema);
