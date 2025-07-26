86375034a35c2e1c23c4c26f65411d5f478177a0
Hem Narayan Chaudhary <hemnarayan.chaudhary.insight@gmail.com>
2025-07-26 10:26:31 +0545

    Add routes for user profile and settingss

--- Frontend/src/constants/routes.js
+++ Frontend/src/constants/routes.js
@@ -1,12 +1,28 @@
-const HOME_ROUTE = "/"
-const ABOUT_ROUTE = "/about"
-const CONTACT_ROUTE = "/contact"
-const STORE_ROUTE = "/store-manager"
-const VENDOR_REGISTER_ROUTE = "/vendor-register"
-const VENDOR_MEMBERSHIP_ROUTE = "/vendor-membership"
-const SHOP_ROUTE = "/shop"
-const LOGIN_ROUTE = "/auth/login"
-const REGISTER_ROUTE = "/auth/register"
+const HOME_ROUTE = "/";
+const ABOUT_ROUTE = "/about";
+const CONTACT_ROUTE = "/contact";
+const STORE_ROUTE = "/store-manager";
+const VENDOR_REGISTER_ROUTE = "/vendor/register";
+const VENDOR_FRONT_STORE_ROUTE = "/vendor/frontstore";
+const SHOP_ROUTE = "/shop";
+// const LOGIN_ROUTE = "/auth/login";
+// const REGISTER_ROUTE = "/auth/register";
+const SINGLE_PRODUCT_PAGE_ROUTE = "/product/";
+const PROFILE_SETTINGS = "/profile/settings";
+const USER_PROFILE = "/user/profile";
 
 
-export {HOME_ROUTE, ABOUT_ROUTE, CONTACT_ROUTE, VENDOR_MEMBERSHIP_ROUTE, VENDOR_REGISTER_ROUTE, STORE_ROUTE, SHOP_ROUTE, LOGIN_ROUTE, REGISTER_ROUTE}
+export {
+  HOME_ROUTE,
+  ABOUT_ROUTE,
+  CONTACT_ROUTE,
+  VENDOR_FRONT_STORE_ROUTE,
+  VENDOR_REGISTER_ROUTE,
+  STORE_ROUTE,
+  SHOP_ROUTE,
+  // LOGIN_ROUTE,
+  // REGISTER_ROUTE,
+  SINGLE_PRODUCT_PAGE_ROUTE,
+  PROFILE_SETTINGS,
+  USER_PROFILE
+};
