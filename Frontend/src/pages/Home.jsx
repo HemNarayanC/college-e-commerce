c3d21bff25ca3f4b70dbdcb41d0b18b3b2453b9d
Hem Narayan Chaudhary <hemnarayan.chaudhary.insight@gmail.com>
2025-07-26 10:14:35 +0545

    Home Page

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
-import HeroSection from '../components/HeroSection'
+import React from "react";
+import HeroSection from "../components/HeroSection";
+import ProductComparison from "../components/CompareBar";
+import FeaturedVendor from "../components/FeaturedVendor";
+import FilterSidebar from "../components/shop/FilterSidebar";
+import ProductList from "../components/products/ProductList";
+import ReviewsTab from "../components/ReviewTabs";
 
 const Home = () => {
   return (
-    <HeroSection />
-  )
-}
+    <>
+      <HeroSection />
+      <FeaturedVendor />
+      <ProductList />
+      <ProductComparison />
+      {/* <FilterSidebar />
+      <ReviewsTab /> */}
+    </>
+  );
+};
 
-export default Home
+export default Home;
