8c899eeec6116c224c211a07e2697478e897de2d
Hem Narayan Chaudhary <hemnarayan.chaudhary.insight@gmail.com>
2025-07-26 10:34:21 +0545

    add icon badge

--- Frontend/src/components/IconBadge.jsx
+++ Frontend/src/components/IconBadge.jsx
@@ -1,14 +1,53 @@
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { getCart } from "../api/cartApi";
+import { setItemCount } from "../redux/cart/cartSlice";
 
-const IconBadge = ({ icon, label, count, badgePosition = "right-2" }) => {
+const IconBadge = ({ icon, label, badgePosition, onClick }) => {
+  const dispatch = useDispatch();
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+  const token = useSelector((state) => state.auth.auth_token);
+  const itemCount = useSelector((state) => state.cartCount.itemCount);
+
+  useEffect(() => {
+    const fetchCartCount = async () => {
+      if (isAuthenticated && token) {
+        try {
+          const response = await getCart(token);
+          console.log("Response for Icon Badge", response)
+          const totalCount = response.reduce(
+            (sum, item) => sum + item.quantity,
+            0
+          );
+          dispatch(setItemCount(totalCount || 0));
+        } catch (error) {
+          console.error("Failed to fetch cart count:", error);
+        }
+      }
+    };
+
+    fetchCartCount();
+  }, [isAuthenticated, token, dispatch]);
+
   return (
     <div className="relative group">
-      <button className="flex flex-col items-center cursor-pointer">
+      <button
+        className="flex flex-col items-center cursor-pointer focus:outline-none"
+        onClick={onClick}
+      >
+        {/* Icon */}
         <span>{icon}</span>
-        <span
-          className={`absolute -top-3 bg-red-500 text-white text-xs rounded-full w-5 h-5 flex items-center justify-center ${badgePosition}`}
-        >
-          {count}
-        </span>
+
+        {/* Badge Count */}
+        {itemCount > 0 && (
+          <span
+            className={`absolute -top-3 ${badgePosition} bg-red-500 text-white text-xs rounded-full w-5 h-5 flex items-center justify-center`}
+          >
+            {itemCount}
+          </span>
+        )}
+
+        {/* Label */}
         <span className="text-xs mt-1">{label}</span>
       </button>
     </div>
