f0b859bd0507a2548d5a3d1069bd9f75611d9d14
Hem Narayan Chaudhary <hemnarayan.chaudhary.insight@gmail.com>
2025-07-26 11:00:19 +0545

    add create token function

--- Backend/src/utils/authToken.js
+++ Backend/src/utils/authToken.js
@@ -2,7 +2,7 @@
 import jwt from 'jsonwebtoken';
 
 const createToken = (data) => {
-   const token = jwt.sign(data, process.env.JWT_SECRET, { expiresIn: 1500})
+   const token = jwt.sign(data, process.env.JWT_SECRET, { expiresIn: 86400})
    return token;
 }
 
