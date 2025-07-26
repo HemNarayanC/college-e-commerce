e48f09e2a831e165ecee8b01b7a73ce72924c650
Hem Narayan Chaudhary <hemnarayan.chaudhary.insight@gmail.com>
2025-07-26 11:01:19 +0545

    database configuration, cors, and api integration

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,13 +1,53 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
+import connectDB from './config/db_connection.js';
+import usersRoute from './routes/userRoutes.js';
+import authRoute from './routes/authRoutes.js';
+import vendorRoute from './routes/vendorRoutes.js';
+import adminRoute from './routes/adminRoutes.js';
+import productRoute from './routes/productRoutes.js';
+import orderRoute from './routes/orderRoutes.js';
+import notificationRoute from './routes/notificationRoutes.js';
+import adminDashboardRoute from './routes/dashboard/adminDashboardRoutes.js';
+import vendorDashboardRoute from './routes/dashboard/vendorDashboardRoutes.js';
+import customerDashboardRoute from './routes/dashboard/customerDashboardRoutes.js';
+import paymentRoutes from './routes/paymentRoutes.js'
+import transactionRoutes from './routes/transactionRoutes.js'
+
+import cookieParser from "cookie-parser";
+
 const app = express();
+
+app.use(cors({
+  origin: "http://localhost:3000",
+  credentials: true
+}));
+
+app.use(express.json({
+  limit: '10mb' // Increase the limit to 50mb
+}));
+app.use(cookieParser());
+
 dotenv.config();
+
+connectDB();
 
 app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
 
-const PORT = process.env.PORT;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+app.use("/api/users", usersRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/vendors", vendorRoute);
+app.use("/api/admin", adminRoute);
+app.use("/api/products", productRoute);
+app.use("/api/orders", orderRoute);
+app.use("/api/notifications", notificationRoute);
+app.use("/api/admin/dashboard", adminDashboardRoute);
+app.use("/api/vendor/dashboard", vendorDashboardRoute);
+app.use("/api/customer/dashboard", customerDashboardRoute);
+app.use("/api/payments", paymentRoutes);
+app.use("/api/transactions", transactionRoutes)
+
+export default app;
