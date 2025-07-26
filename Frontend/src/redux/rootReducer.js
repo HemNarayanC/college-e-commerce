365c645a6072d165f106a8d86d061e7828c09262
Hem Narayan Chaudhary <hemnarayan.chaudhary.insight@gmail.com>
2025-07-26 11:40:33 +0545

    removed wishlist

--- Frontend/src/redux/rootReducer.js
+++ Frontend/src/redux/rootReducer.js
@@ -3,11 +3,9 @@ import { combineReducers } from "@reduxjs/toolkit";
 import authReducer from "../redux/auth/authSlice"
 import compareReducer from "../redux/product/compareProduct";
 import cartCountReducer from "../redux/cart/cartSlice"
-import wishlistReducer from "../redux/wishlist/wishListsSlice";
 
 const rootReducer = combineReducers({
   cartCount: cartCountReducer,
-  wishlist: wishlistReducer,
   compare: compareReducer,
   auth: authReducer,
 });
