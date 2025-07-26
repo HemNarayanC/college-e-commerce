25c88ffbe61d039f35008991b0daa9e111726720
Hem Narayan Chaudhary <hemnarayan.chaudhary.insight@gmail.com>
2025-07-26 10:51:38 +0545

    add isFeatured and isPlatformSeller

--- Backend/src/models/Vendor.js
+++ Backend/src/models/Vendor.js
@@ -31,10 +31,12 @@ const VendorSchema = new mongoose.Schema({
   ratingAvg: { type: Number, default: 0, min: 0, max: 5 },
   status: {
     type: String,
-    enum: ["approved", "pending", "banned"],
+    enum: ["approved", "pending", "suspended", "rejected"],
     default: "pending",
   },
   commissionRate: { type: Number, default: 0.1, min: 0, max: 1 },
+  isFeatured: { type: Boolean, default: true },
+  isPlatformSeller: { type: Boolean, default: false },
   createdAt: { type: Date, default: Date.now },
 });
 
